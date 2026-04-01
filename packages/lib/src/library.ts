3413237cca531f33c54c144f352eb627dfa835d3
Marco Beier <marco-beier@t-online.de>
2026-04-01 22:14:02 +0200

    chore: scaffold library package with UI5 tooling

---
+++ packages/lib/src/library.ts
@@ -0,0 +1,16 @@
+import Lib from "sap/ui/core/Lib";
+import "sap/ui/core/library";
+
+const library = Lib.init({
+	apiVersion: 2,
+	name: "ui5.model.signal",
+	version: "${version}",
+	dependencies: ["sap.ui.core"],
+	types: [],
+	interfaces: [],
+	controls: [],
+	elements: [],
+	noLibraryCSS: true,
+});
+
+export default library;
